b85a5696726eedfb03f3f9ddaa81cad21c218c63
wtier-cs <ierodriguez2@buffs.wtamu.edu>
2025-01-27 19:39:56 -0600

    Upload Homework1.cs

---
+++ Homework1.cs
@@ -0,0 +1,12 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float x = 2.5f;
+        float y = 3.3f;
+        float z = 4.0f*x*x + 3.0f*y;
+        Console.WriteLine($"X = {x}, Y = {y}\nThe value of Z is: {z}");
+    }
+}
